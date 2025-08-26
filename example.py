4a19341881fdb8b48a1bdc935e807019ddf7fd38
Lorri Rao <zouyoo@outlook.com>
2025-08-26 14:49:52 -0700

    fix missing rank

--- example.py
+++ example.py
@@ -28,6 +28,7 @@ def run_all_gather(rank, world_size):
         # Main profiled section
         with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
             # Each process creates a tensor with its rank
+            device=torch.device(f"cuda:{rank}")
             tensor = torch.full([12, 1024], rank).to(device)
             # Prepare a list of tensors to receive the gathered data
             tensor_list = [torch.zeros_like(tensor) for _ in range(world_size)]
